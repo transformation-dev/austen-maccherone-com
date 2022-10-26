3536dda2f8512f152d3ce06041a5c4c0cc195637
Larry Maccherone <Larry@Maccherone.com>
2022-10-26 19:19:38 +0000

    Added SliderGallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,17 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import SliderGallery from '../components/SliderGallery.svelte';
+	import SliderGallery from '$lib/components/SliderGallery.svelte';
 
 	import magpie from '$lib/images/magpie.jpg';
 	import flick from '$lib/images/flick.jpg';
 	import blackbird from '$lib/images/blackbird.jpg';
 
 	let imgArray = [
-		{ src: magpie, text: "Magpie" },
-		{ src: flick, text: "Flick" },
-		{ src: blackbird, text: "Blackbird" },
+		{ src: magpie, text: 'Magpie' },
+		{ src: flick, text: 'Flick' },
+		{ src: blackbird, text: 'Blackbird' }
 	];
-
 </script>
 
 <svelte:head>
@@ -22,14 +21,9 @@
 </svelte:head>
 
 <section>
-
-	<h1>
-		Welcome
-	</h1>
+	<h1>Welcome</h1>
 
 	<SliderGallery {imgArray} />
-
-
 </section>
 
 <style>
@@ -43,21 +37,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
