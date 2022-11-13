3c3e122df4af0882aad63a204ef89aff1f04717d
austenmaccherone <113698461+austenmaccherone@users.noreply.github.com>
2022-11-13 14:29:12 -0500

    update to text on main 3 pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import PhotoCarousel from '$lib/components/PhotoCarousel.svelte';
 
 	let photos = [
-		{ src: '/images/AArt1.jpg', text: 'Flowers (ink)' },
+		{ src: '/images/AArt1.jpg', text: 'Flowers (ink) ' },
 		{ src: '/images/AArt2.jpg', text: 'Real Life Perspective (stained with coffee)' },
 		{ src: '/images/A_Art3.jpg', text: 'Still Life (charcoal on grey canson paper)' },
 		{ src: '/images/AArt4.jpg', text: 'Color Wheel (acryllic on bristol) '},
@@ -21,9 +21,8 @@
 	<PhotoCarousel {photos} />
 </section>
 
-<h1 id="description">Hello </h1>
-<body> My passion for creation and innovation is what drives me to work and the results never fail to shock and amaze me. I love nature, it is something that is fabricated within all of us. There is a power in the intuition carved into the mind. My opinionated personality and exaggerated nature are what bring my ideas to life. 
-</body>
+<h1 id="description">Fine Art</h1>
+
 
 <style>
 	section {
